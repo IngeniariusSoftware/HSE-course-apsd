639bef715dbbfbe3058ae1427eff04bbf981f3e0
IngeniariusSoftware <kyngfy-panda@yandex.ru>
2018-09-24 09:30:21 +0500

    Добавлена реализация алгоритма для составления ленточного расписания

--- Tape-schedule/Program.cs
+++ Tape-schedule/Program.cs
@@ -10,20 +10,20 @@ namespace Tape_schedule
     {
         public static int VvodNonNegative()
         {
-            Console.WriteLine("\n\tПожалуйста, введите целое неотрицательное число:");
+            Console.WriteLine("\n\tПожалуйста, введите натуральное число:");
             bool rightNonNegative;
             do
             {
                 rightNonNegative = int.TryParse(Console.ReadLine(), out int numberZahlen);
-                if (numberZahlen < 0)
+                if (numberZahlen < 1)
                 {
                     rightNonNegative = false;
                 }
 
                 if (!rightNonNegative)
                 {
-                    Console.WriteLine("\n\t\aК сожалению, вы можете ввести только неотрицательное число.");
-                    Console.WriteLine("\n\tПопробуйте ввести число ещё раз:.");
+                    Console.WriteLine("\n\t\aК сожалению, вы можете ввести только натуральное число.");
+                    Console.WriteLine("\n\tПопробуйте ввести число ещё раз:");
                 }
                 else
                 {
@@ -37,19 +37,68 @@ namespace Tape_schedule
 
         static void Main()
         {
-            Console.WriteLine((int)'A');
-
             Console.WriteLine("\n\tВвод количества работ");
             int workCount = VvodNonNegative();
-            int [] workTime = new int[workCount];
+            int[] workTime = new int[workCount];
             for (int i = 0; i < workCount; i++)
             {
-                Console.WriteLine("\n\tВвод количества работ");
+                Console.WriteLine("\n\tВведите время выполенния работы {0}", (char)(65 + i));
                 workTime[i] = VvodNonNegative();
-
             }
 
+            Console.WriteLine("\n\tВвод количества работников");
+            int workerCount = VvodNonNegative();
+            int maxWorkTime;
+            if (workTime.Last() > workTime.Sum() / workerCount)
+            {
+                maxWorkTime = workTime.Last();
 
+            }
+            else
+            {
+                maxWorkTime = workTime.Sum() / workerCount;
+            }
+
+            int j = -1;
+            for (int i = 0; i < workerCount; i++)
+            {
+                if (j == -1 || workTime[j] == 0)
+                {
+                    j = Array.BinarySearch(workTime, workTime.Max());
+                }
+
+                Console.Write("\tРаботник {0}:", i + 1);
+                int deltaWorkTime = maxWorkTime;
+                while (workTime.Sum() > 0 && deltaWorkTime > 0)
+                {
+                    if (deltaWorkTime >= workTime[j])
+                    {
+                        Console.Write(
+                            " {0} ({1} - {2});",
+                            (char)(65 + j),
+                            maxWorkTime - deltaWorkTime,
+                            maxWorkTime - deltaWorkTime + workTime[j]);
+                        deltaWorkTime -= workTime[j];
+                        workTime[j] = 0;
+                        j--;
+                    }
+                    else
+                    {
+                        Console.Write(
+                            " {0} ({1} - {2});",
+                            (char)(65 + j),
+                            maxWorkTime - deltaWorkTime,
+                            maxWorkTime);
+                        workTime[j] -= deltaWorkTime;
+                        deltaWorkTime = 0;
+                    }
+                }
+
+                Console.WriteLine("\n");
+            }
+
+            Console.WriteLine("\tДля завершения работы нажите любую клавишу...");
+            Console.ReadKey();
         }
     }
 }
