a90d5b0ea8f0d5fe3addbf33497c78603ae925a5
Максим <kyngfy-panda@yandex.ru>
2018-11-23 23:50:08 +0500

    Добавил генератор тестов для проверки алгоритма Форда Фолкерсона

---
+++ Flow_test_generator/Data/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Data")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Data")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d24ed4f6-de26-46cb-9188-ba2e801ca4a3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Flow_test_generator/Data/UnitTestAlgorithm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Data
+{
+    using System.IO;
+
+    [TestClass]
+    public class UnitTestAlgorithm
+    {
+
+
+    }
+}
