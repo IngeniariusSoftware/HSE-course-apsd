84cdcc58a0fb093a57e260b26a25e0766ccb4511
Максим <kyngfy-panda@yandex.ru>
2018-12-05 00:20:38 +0500

    Добавил ввод стоимости, добавил расчет удельной стоимости

--- Knapsack problem/Data/Program.cs
+++ Knapsack problem/Data/Program.cs
@@ -9,30 +9,36 @@
 
     class Program
     {
-        private static List<(string itemName, int weight, int count)> ParserItems()
+        private static List<(string itemName, double cost, double weight, int count, double value)> ParserItems()
         {
-            var items = new List<(string itemName, int weight, int count)>();
+            var items = new List<(string itemName, double cost, double weight, int count, double value)>();
             StreamReader input = new StreamReader("input.txt");
             while (!input.EndOfStream)
             {
                 string[] tokens = input.ReadLine().Split(' ');
                 switch (tokens.Length)
                 {
-                    case 1:
-                        {
-                            items.Add((items.Count.ToString(), int.Parse(tokens[0]), 1));
-                            break;
-                        }
-
                     case 2:
                         {
-                            items.Add((items.Count.ToString(), int.Parse(tokens[0]), int.Parse(tokens[1])));
+                            items.Add(
+                                (items.Count.ToString(), double.Parse(tokens[0]), double.Parse(tokens[1]), 1,
+                                    double.Parse(tokens[0]) / double.Parse(tokens[1])));
                             break;
                         }
 
                     case 3:
                         {
-                            items.Add((tokens[0], int.Parse(tokens[1]), int.Parse(tokens[2])));
+                            items.Add(
+                                (items.Count.ToString(), double.Parse(tokens[0]), double.Parse(tokens[1]),
+                                    int.Parse(tokens[2]), double.Parse(tokens[0]) / double.Parse(tokens[1])));
+                            break;
+                        }
+
+                    case 4:
+                        {
+                            items.Add(
+                                (tokens[0], double.Parse(tokens[1]), double.Parse(tokens[2]), int.Parse(tokens[2]),
+                                    double.Parse(tokens[1]) / double.Parse(tokens[2])));
                             break;
                         }
                 }
@@ -45,7 +51,7 @@
         static void Main()
         {
             var items = ParserItems();
-            
+            items.Sort();
 
         }
     }
